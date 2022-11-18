9903df37d627caa6f2f351f6e37960b83fe83539
Xiangyi Lin MacBook Pro VSCode <xil176@ucsd.edu>
2022-11-18 13:45:44 -0800

    submit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Lab 8 - Starter
+Write by Xiangyi Lin
+
+1) Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
+
+    
+    I will fit my automated tests within a Github action that runs whenever code is pushed, because we can not make sure if every pushing code is done for testing, and action can reject push if it fail the test.
+
+
+2) Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+
+    No. End to end test better suited for testing complex interactions rather than simple output of function. It is more suitable for unit test.
+
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+
+    No. "Messaging" applications involve complex interactions and are more suitable for end to end testing.
+
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+
+    Yes. This is just a simple test and does not involve complex interactions, so it can be done with unit tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,78 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+describe('function isPhoneNumber', () => {
+    it('123-123-1234 is a phone number', async () => {
+        expect(functions.isPhoneNumber('123-123-1234')).toBe(true);
+    });
+    it('(123)-123-1234 is a phone number', async () => {
+        expect(functions.isPhoneNumber('123-123-1234')).toBe(true);
+    });
+    it('number is NOT a phone number', async () => {
+        expect(functions.isPhoneNumber('number')).toBe(false);
+    });
+    it('123-abc-1234 is NOT a phone number', async () => {
+        expect(functions.isPhoneNumber('123-abc-1234')).toBe(false);
+    });
+});
+
+describe('function isEmail', () => {
+    it('123abc@gmail.com is a email', async () => {
+        expect(functions.isEmail('123abc@gmail.com')).toBe(true);
+    });
+    it('111@sd.sd is a email', async () => {
+        expect(functions.isEmail('111@sd.sd')).toBe(true);
+    });
+    it('mail is NOT a email', async () => {
+        expect(functions.isEmail('mail')).toBe(false);
+    });
+    it('123-abc-1234gmail.com is NOT a email', async () => {
+        expect(functions.isEmail('123-abc-1234gmail.com')).toBe(false);
+    });
+});
+
+describe('function isStrongPassword', () => {
+    it('PaSsWoRd is a strong password', async () => {
+        expect(functions.isStrongPassword('PaSsWoRd')).toBe(true);
+    });
+    it('A1b2C3d4 is a strong password', async () => {
+        expect(functions.isStrongPassword('A1b2C3d4')).toBe(true);
+    });
+    it('aa is NOT a strong password', async () => {
+        expect(functions.isStrongPassword('aa')).toBe(false);
+    });
+    it('111 is NOT a strong password', async () => {
+        expect(functions.isStrongPassword('111')).toBe(false);
+    });
+});
+
+
+describe('function isDate', () => {
+    it('1/1/2022 is a date', async () => {
+        expect(functions.isDate('1/1/2022')).toBe(true);
+    });
+    it('01/01/2022 is a date', async () => {
+        expect(functions.isDate('01/01/2022')).toBe(true);
+    });
+    it('010122 is NOT a date', async () => {
+        expect(functions.isDate('010122')).toBe(false);
+    });
+    it('0101 is NOT a date', async () => {
+        expect(functions.isDate('0101')).toBe(false);
+    });
+});
+
+describe('function isHexColor', () => {
+    it('#ffffff is a hex color', async () => {
+        expect(functions.isHexColor('#ffffff')).toBe(true);
+    });
+    it('#123abc is a hex color', async () => {
+        expect(functions.isHexColor('#123abc')).toBe(true);
+    });
+    it('#12 is NOT a hex color', async () => {
+        expect(functions.isHexColor('#12')).toBe(false);
+    });
+    it('gks is NOT a hex color', async () => {
+        expect(functions.isHexColor('gks')).toBe(false);
+    });
+});
